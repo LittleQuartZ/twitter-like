0ed04b7fff2ef6925cafeb9ea61cb4edaf871f7f
LittleQuartZ <58659387+LittleQuartZ@users.noreply.github.com>
2022-08-23 21:35:18 +0700

    update authSchema

--- src/utils/validation/auth.ts
+++ src/utils/validation/auth.ts
@@ -5,8 +5,8 @@ export const loginSchema = z.object({
     .string({
       required_error: 'Username is required',
     })
-    .min(5, {
-      message: 'Username must be at least 5 characters',
+    .min(1, {
+      message: 'Username cannot be empty',
     })
     .max(20, {
       message: 'Username must be less than 20 characters',
@@ -14,9 +14,7 @@ export const loginSchema = z.object({
     .regex(/^[A-Za-z0-9_]+$/, {
       message: 'Username must only contains letters and numbers',
     }),
-  password: z
-    .string()
-    .min(8, { message: 'Password must be at least 8 characters' }),
+  password: z.string().min(1, { message: 'Password cannot be empty' }),
 })
 
 export const registerSchema = loginSchema.extend({})
