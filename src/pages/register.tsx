c70f2999017c66df5ff216d5f2e074b469a2f6d2
LittleQuartZ <58659387+LittleQuartZ@users.noreply.github.com>
2022-08-23 21:36:29 +0700

    style register and signin page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,4 +1,4 @@
-import { IRegister, registerSchema } from '@/utils/validation/auth'
+import { ILogin, IRegister, registerSchema } from '@/utils/validation/auth'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
@@ -9,24 +9,53 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { trpc } from '@/utils/trpc'
 
+import * as LabelPrimitive from '@radix-ui/react-label'
+
+const inputClassName =
+  'p-4 w-full max-w-full resize-none bg-indigo-200 rounded-md placeholder:text-indigo-400 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition'
+const labelClassName =
+  'select-none cursor-text absolute text-indigo-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-indigo-200 px-4 peer-focus:px-4 peer-placeholder-shown:text-indigo-400 peer-focus:text-indigo-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1'
+
+const Label = LabelPrimitive.Root
+
 const Register: NextPage = () => {
   const router = useRouter()
 
-  const { register, handleSubmit, formState } = useForm<IRegister>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IRegister>({
     resolver: zodResolver(registerSchema),
+    reValidateMode: 'onChange',
   })
 
-  const { mutateAsync } = trpc.useMutation('user.create')
+  const { mutateAsync, error } = trpc.useMutation('user.create')
 
   const onSubmit = useCallback(
     async (data: IRegister) => {
       const result = await mutateAsync(data)
+
       if (result.username) {
         router.push('/signin')
       }
     },
     [mutateAsync, router]
   )
+
+  let errorElement
+
+  if (Object.values(errors).length !== 0) {
+    errorElement = Object.values(errors).map(({ message }, index) => (
+      <p className='w-2/3 p-4 rounded-md bg-red-300' key={index}>
+        {message}
+      </p>
+    ))
+  } else if (error) {
+    errorElement = (
+      <p className='w-2/3 p-4 rounded-md bg-red-300'>{error.shape?.message}</p>
+    )
+  }
 
   return (
     <div>
@@ -35,31 +64,50 @@ const Register: NextPage = () => {
         <meta name='description' content='Register page' />
       </Head>
 
-      <main>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div>
-            <h2>Create an account!</h2>
+      <main className='w-full h-screen bg-indigo-100 flex flex-col items-center gap-6 justify-center'>
+        <h2 className='text-2xl font-bold'>Create an account!</h2>
+        <form
+          className='flex flex-col gap-4 w-2/3'
+          onSubmit={handleSubmit(onSubmit)}>
+          <fieldset className='relative'>
             <input
+              id='username'
+              className={inputClassName + ' peer'}
               type='text'
-              placeholder='Username with no symbols'
+              placeholder='  '
+              aria-placeholder=''
               {...register('username')}
             />
+            <Label htmlFor='username' className={labelClassName}>
+              Username with no symbols
+            </Label>
+          </fieldset>
+          <fieldset className='relative'>
             <input
+              id='password'
+              className={inputClassName + ' peer'}
               type='password'
-              placeholder='A secret password'
+              placeholder='   '
+              aria-placeholder=''
               {...register('password')}
             />
-            <div>
-              <Link href='/signin'>Go to login</Link>
-              <button type='submit'>Register</button>
-            </div>
-          </div>
+            <Label htmlFor='password' className={labelClassName}>
+              A secret password
+            </Label>
+          </fieldset>
+          <button
+            type='submit'
+            className='bg-indigo-300 text-indigo-900 px-4 py-2 rounded-md flex-1 font-bold w-full focus:ring-2 focus:ring-indigo-500 focus:outline-none'>
+            Register
+          </button>
+          <Link href='/signin'>
+            <a className='text-indigo-500 focus:underline focus:outline-none focus:font-bold ml-auto'>
+              Already have an account? Login
+            </a>
+          </Link>
         </form>
+        {errorElement}
       </main>
-
-      {Object.values(formState.errors).map(({ message }, index) => (
-        <p key={index}>{message}</p>
-      ))}
     </div>
   )
 }
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -6,6 +6,14 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { signIn } from 'next-auth/react'
 import Head from 'next/head'
 import Link from 'next/link'
+import * as LabelPrimitive from '@radix-ui/react-label'
+
+const Label = LabelPrimitive.Root
+
+const inputClassName =
+  'p-4 w-full max-w-full resize-none bg-indigo-200 rounded-md placeholder:text-indigo-400 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition'
+const labelClassName =
+  'select-none cursor-text absolute text-indigo-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-indigo-200 px-4 peer-focus:px-4 peer-placeholder-shown:text-indigo-400 peer-focus:text-indigo-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1'
 
 const SignIn: NextPage = () => {
   const { register, handleSubmit } = useForm<ILogin>({
@@ -23,27 +31,49 @@ const SignIn: NextPage = () => {
         <meta name='description' content='Sign-In page' />
       </Head>
 
-      <main>
-        <form onSubmit={handleSubmit(onSubmit)}>
+      <main className='w-full h-screen bg-indigo-100 flex flex-col items-center gap-6 justify-center'>
+        <h2 className='text-2xl font-bold'>Welcome Back!</h2>
+        <form
+          className='flex flex-col gap-4 w-2/3'
+          onSubmit={handleSubmit(onSubmit)}>
+          <fieldset className='relative'>
+            <input
+              type='text'
+              id='username'
+              placeholder='  '
+              aria-placeholder=''
+              className={inputClassName + ' peer'}
+              {...register('username')}
+            />
+            <Label htmlFor='username' className={labelClassName}>
+              Username
+            </Label>
+          </fieldset>
+          <fieldset className='relative'>
+            <input
+              type='password'
+              id='password'
+              placeholder='  '
+              aria-placeholder=''
+              className={inputClassName + ' peer'}
+              {...register('password')}
+            />
+            <Label htmlFor='password' className={labelClassName}>
+              Password
+            </Label>
+          </fieldset>
           <div>
-            <div>
-              <h2>Welcome Back!</h2>
-              <input
-                type='text'
-                placeholder='type your username'
-                {...register('username')}
-              />
-              <input
-                type='password'
-                placeholder='type your password'
-                {...register('password')}
-              />
-              <div>
-                <Link href='/register'>Go to Register</Link>
-                <button type='submit'>Login</button>
-              </div>
-            </div>
+            <button
+              type='submit'
+              className='bg-indigo-300 text-indigo-900 px-4 py-2 rounded-md flex-1 font-bold w-full focus:ring-2 focus:ring-indigo-500 focus:outline-none'>
+              Login
+            </button>
           </div>
+          <Link href='/register'>
+            <a className='text-indigo-500 focus:underline focus:outline-none focus:font-bold ml-auto'>
+              Don't have an account? Register
+            </a>
+          </Link>
         </form>
       </main>
     </div>
