7384576d3fc27a2c611ff37ce625be1b2dacb04e
LittleQuartZ <58659387+LittleQuartZ@users.noreply.github.com>
2022-08-24 11:16:28 +0700

    change color register page

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -12,9 +12,9 @@ import { trpc } from '@/utils/trpc'
 import * as LabelPrimitive from '@radix-ui/react-label'
 
 const inputClassName =
-  'p-4 w-full max-w-full resize-none bg-indigo-200 rounded-md placeholder:text-indigo-400 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition'
+  'p-4 w-full max-w-full resize-none bg-blue-200 rounded-md placeholder:text-blue-400 focus:ring-2 focus:ring-blue-500 focus:outline-none transition'
 const labelClassName =
-  'select-none cursor-text absolute text-indigo-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-indigo-200 px-4 peer-focus:px-4 peer-placeholder-shown:text-indigo-400 peer-focus:text-indigo-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1'
+  'select-none cursor-text absolute text-blue-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-blue-200 px-4 peer-focus:px-4 peer-placeholder-shown:text-blue-400 peer-focus:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1'
 
 const Label = LabelPrimitive.Root
 
@@ -64,7 +64,7 @@ const Register: NextPage = () => {
         <meta name='description' content='Register page' />
       </Head>
 
-      <main className='w-full h-screen bg-indigo-100 flex flex-col items-center gap-6 justify-center'>
+      <main className='w-full h-screen bg-blue-100 flex flex-col items-center gap-6 justify-center'>
         <h2 className='text-2xl font-bold'>Create an account!</h2>
         <form
           className='flex flex-col gap-4 w-2/3'
@@ -97,11 +97,11 @@ const Register: NextPage = () => {
           </fieldset>
           <button
             type='submit'
-            className='bg-indigo-300 text-indigo-900 px-4 py-2 rounded-md flex-1 font-bold w-full focus:ring-2 focus:ring-indigo-500 focus:outline-none'>
+            className='bg-blue-300 text-blue-900 px-4 py-2 rounded-md flex-1 font-bold w-full focus:ring-2 focus:ring-blue-500 focus:outline-none'>
             Register
           </button>
           <Link href='/signin'>
-            <a className='text-indigo-500 focus:underline focus:outline-none focus:font-bold ml-auto'>
+            <a className='text-blue-500 focus:underline focus:outline-none focus:font-bold ml-auto'>
               Already have an account? Login
             </a>
           </Link>
